5f4dbe573d39caa60e536d76437e69a666a4f04f
Jay Prakash Kumar <jay.kasyap@live.com>
2018-11-04 12:29:17 +0530

    Establish MongoDB Connection

--- config/appConfig.js
+++ config/appConfig.js
@@ -3,9 +3,9 @@ let appConfig = {};
 appConfig.port = 3000;
 appConfig.allowedCorsOrigin = "*";
 appConfig.env = "Dev";
-// appConfig.db = {
-//     uri = 'mongoDB://appDB',
-// }
+appConfig.db = {
+    uri: 'mongodb://127.0.0.1:27017/appDB',
+}
 appConfig.apiVersion = '/api/v1';
 
 // Create mdoule Export(Object) to use config in our Application some how
@@ -13,6 +13,6 @@ module.exports = {
     port: appConfig.port,
     allowedCorsOrigin:appConfig.allowedCorsOrigin,
     env:appConfig.env,
-    // db:appConfig.db,
+    db:appConfig.db,
     apiVersion:appConfig.apiVersion
 }// ENd Module Exports
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // This is needed to export our express Application
 const express = require('express')
 const fs = require('fs')
+const mongoose = require ('mongoose'); // Include Mongoose
 
 //Include Module Exports from Config File
 const appConfig  = require('./config/appConfig')
@@ -19,7 +20,25 @@ fs.readdirSync(routesPath).forEach(function(file){
         let route = require(routesPath+ '/'+file)
         route.setRouter(app);
     }
-})
-app.listen(appConfig.port,() =>
+});
+app.listen(appConfig.port,() => {
     console.log('App running at port number 3000')
-)
+    //create mongo db connection here
+    let db = mongoose.connect(appConfig.db.uri, {useNewUrlParser:true});
+});
+
+// Handling Mongoose connection Error
+mongoose.connection.on('error', function(err){
+    console.log('Database connection error');
+    console.log(err);
+});// End of Mongoose connection Error
+
+// Handle Mongoose success event
+mongoose.connection.on('open', function(err){
+    if(err){
+        console.log('Database connection errorr');
+        console.log(err);
+    }else{
+        console.log('Connection Establish Successfully');
+    }
+});// End mongoose connection open Handler
